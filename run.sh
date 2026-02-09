a33a84d2bd100f2e84fa98f3f9ada9e62d95ec8e
George Kaceli <georgekaceli8@gmail.com>
2026-02-09 11:19:56 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
 # In this example, we show how to train SimCSE on unsupervised Wikipedia data.
 # about how to use PyTorch's distributed data parallel.
 
-python train.py \
+python -m torch.distributed.launch --nproc_per_node=5 train.py \
   --train_file data/wiki1m_for_simcse.txt \
   --model_name_or_path bert-base-uncased \
-  --num_train_epochs 1 \
-  --per_device_train_batch_size 64 \
+  --num_train_epochs 100 \
+  --per_device_train_batch_size 16 \
+  gradient_accumulation_steps 8 \
   --learning_rate 3e-5 \
   --max_seq_length 32 \
   --pooler_type avg \
   --temp 0.05 \
+  --ilcl_sa \
+  --ilcl_layers 4 8 12 \
+  --ilcl_weight 0.4 \
   --do_neg \
   --mlp_only_train \
   --do_train \
